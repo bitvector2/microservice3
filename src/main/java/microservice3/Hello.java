d68704f612fa022f3848fa0e232da82d3ae6ad6d
Steven Logue <bitvector2@gmail.com>
2016-12-23 20:32:41 -0800

    Initial Commit

---
+++ src/main/java/microservice3/Hello.java
@@ -0,0 +1,9 @@
+package microservice3;
+
+import static spark.Spark.get;
+
+public class Hello {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
