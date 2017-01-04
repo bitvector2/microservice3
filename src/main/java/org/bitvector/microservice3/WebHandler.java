db59d125b8a99c6e0bc44ed7cab7435d7fc2ef47
Steven Logue <bitvector2@gmail.com>
2017-01-04 14:42:26 -0800

    fleshed out WebHandlers

--- src/main/java/org/bitvector/microservice3/WebHandler.java
+++ src/main/java/org/bitvector/microservice3/WebHandler.java
@@ -3,9 +3,10 @@ package org.bitvector.microservice3;
 import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.Request;
+import spark.Response;
 
-import static spark.Spark.get;
-import static spark.Spark.stop;
+import static spark.Spark.*;
 
 public class WebHandler {
     private Logger logger;
@@ -19,15 +20,50 @@ public class WebHandler {
         gson = new Gson();
         dbHandler = dbh;
 
-        get("/hello", (request, response) -> {
-            response.status(200);
-            response.type("application/json");
-            return gson.toJson(dbHandler.getAllProducts());
-        });
+        // Route Wiring
+        get("/products", this::getAllProducts);
+        get("/products/:ID", this::getProductById);
+        put("/products/:ID", this::putProductById);
+        post("/products", this::postProduct);
+        delete("/products/:ID", this::deleteProductById);
     }
 
     public void close() {
         logger.info("Stopping WebHandler...");
         stop();
     }
+
+    private String getAllProducts(Request request, Response response) {
+        response.status(200);
+        response.type("application/json");
+        return gson.toJson(dbHandler.getAllProducts());
+    }
+
+    private String getProductById(Request request, Response response) {
+        response.status(200);
+        response.type("application/json");
+        Integer id = Integer.parseInt(request.params(":ID"));
+        return gson.toJson(dbHandler.getProductById(id));
+    }
+
+    private String putProductById(Request request, Response response) {
+        response.status(200);
+        response.type("application/json");
+        Integer id = Integer.parseInt(request.params(":ID"));
+        return gson.toJson(true);
+    }
+
+    private String postProduct(Request request, Response response) {
+        response.status(200);
+        response.type("application/json");
+        return gson.toJson(true);
+    }
+
+    private String deleteProductById(Request request, Response response) {
+        response.status(200);
+        response.type("application/json");
+        Integer id = Integer.parseInt(request.params(":ID"));
+        return gson.toJson(true);
+    }
+
 }
