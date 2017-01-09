7f2f9d30d8c8bf5c56f307aca5e98e80e7e094b5
Steven Logue <bitvector2@gmail.com>
2017-01-09 11:02:25 -0800

    reverted to latest production release of Hazelcast and added specific heap size for startup

--- microservice3.sh
+++ microservice3.sh
@@ -2,7 +2,7 @@
 
 if [ $(uname -m) == "armv7l" ];
 then
-    java -d32 -server -XX:+UseG1GC -jar target/microservice3-1.0-SNAPSHOT.jar
+    java -d32 -server -XX:+UseG1GC -Xms768m -Xmx768m -jar target/microservice3-1.0-SNAPSHOT.jar
 else
-    java -d64 -server -XX:+UseG1GC -jar target/microservice3-1.0-SNAPSHOT.jar
+    java -d64 -server -XX:+UseG1GC -Xms768m -Xmx768m -jar target/microservice3-1.0-SNAPSHOT.jar
 fi
